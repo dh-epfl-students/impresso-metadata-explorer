3d7a4b805da115fa85f622dd0faf437ad2a62ef3
Justine Weber <justine.weber@epfl.ch>
2019-11-05 11:32:46 +0100

    Axis titles setup (new function display_setup).

--- visualization.py
+++ visualization.py
@@ -7,9 +7,15 @@ import pandas as pd
 from typing import Iterable
 
 
-def plot_issues_time_id(time_gran: str, start_date: int, end_date: int, np_ids: Iterable = None,
-                        country: str = None, df: pd.core.frame.DataFrame = None, batch_size: int = None,
-                        ppty: str = None, ppty_value: str = None) -> None:
+def plot_issues_time_id(time_gran: str,
+                        start_date: int = None,
+                        end_date: int = None,
+                        np_ids: Iterable = None,
+                        country: str = None,
+                        df: pd.core.frame.DataFrame = None,
+                        ppty: str = None,
+                        ppty_value: str = None,
+                        batch_size: int = None) -> None:
     """
     General plotting function for issues frequency analysis (histogram).
     :param time_gran: granularity in time, either 'year' or 'decade'
@@ -18,9 +24,9 @@ def plot_issues_time_id(time_gran: str, start_date: int, end_date: int, np_ids: 
     :param np_ids: list (or pandas series) of newspapers ids on which to focus
     :param country: selected country code
     :param df: original data frame on which to build the histogram
-    :param batch_size: maximum number of newspapers represented on a single plot
     :param ppty: selected property on which to filter newspapers
     :param ppty_value: property value corresponding to the selected property
+    :param batch_size: maximum number of newspapers represented on a single plot
     :return: Nothing, but plots the histogram(s) of issue frequency.
     """
     issues_df = df
@@ -43,12 +49,16 @@ def plot_issues_time_id(time_gran: str, start_date: int, end_date: int, np_ids: 
 
     # if batch_size not specified : plot all newspapers on the same figure
     if batch_size is None:
-        sns.catplot(x=time_gran, y="count", hue="newspaper_id", kind="bar", data=count_df, height=5, aspect=2)
+        g = sns.catplot(x=time_gran, y="count", hue="newspaper_id", kind="bar", data=count_df, height=5, aspect=2)
+        display_setup(g, display_x_label=False, y_label='Number of issues',
+                      title='Issue frequency per newspaper, through time.')
 
     # else plot by batches (no intelligent batching is done)
     else:
         assert (0 < batch_size and batch_size < 20), "Batch size must be between 1 and 19."
-        catplot_by_batch_np(count_df, np_ids_filtered, 'decade', 'count', 'newspaper_id', batch_size)
+        catplot_by_batch_np(count_df, np_ids_filtered, 'decade', 'count', 'newspaper_id', batch_size,
+                            display_x_label=False, y_label='Number of issues',
+                            title='Issue frequency per newspaper, through time.')
 
 
 def catplot_by_batch_np(df: pd.core.frame.DataFrame,
@@ -57,7 +67,12 @@ def catplot_by_batch_np(df: pd.core.frame.DataFrame,
                         yp: str,
                         huep: str,
                         max_cat: int = 5,
-                        rotation: int = None) -> None:
+                        rotation: int = None,
+                        display_x_label: bool = True,
+                        display_y_label: bool = True,
+                        x_label: str = None,
+                        y_label: str = None,
+                        title: str = None) -> None:
     """
     Helper function for plotting by batches
     :param df: data frame to plot (no processing done on it)
@@ -67,6 +82,11 @@ def catplot_by_batch_np(df: pd.core.frame.DataFrame,
     :param huep: hue variable
     :param max_cat: maximum number of categories represented on a single plot
     :param rotation: rotation for the labels on the x axis (optional)
+    :param display_x_label: set to False if x axis title should be hidden
+    :param display_y_label: set to False if y axis title should be hidden
+    :param x_label: specify the title you want to set for the x axis
+    :param y_label: specify the title you want to set for the y axis
+    :param title: specify plot title
     :return: Nothing, only plots.
     """
     if len(df.newspaper_id.unique()) > max_cat:
@@ -78,8 +98,46 @@ def catplot_by_batch_np(df: pd.core.frame.DataFrame,
     for i, b in enumerate(np_batch):
         batch = filter_df_by_np_id(df, b)
         g = sns.catplot(x=xp, y=yp, hue=huep, kind="bar", data=batch, height=5, aspect=2)
-        if rotation is not None:
-            g.set_xticklabels(rotation=rotation)
+        display_setup(g, rotation, display_x_label, display_y_label, x_label, y_label, title)
+
+
+def display_setup(g: sns.axisgrid.FacetGrid,
+                  rotation: int = None,
+                  display_x_label: bool = True,
+                  display_y_label: bool =True,
+                  x_label: str = None,
+                  y_label: str = None,
+                  title: str = None) -> None:
+    """
+    Common setup of seaborn plots : set titles and other stuff.
+    :param g: the seaborn FacetGrid on which to do settings.
+    :param rotation: degree of rotation for the x axis labels.
+    :param display_x_label: boolean, set to False if you want to hide the x axis title. (default is True)
+    :param display_y_label: boolean, set to False if you want to hide the y axis title. (default is True)
+    :param x_label: specify particular x axis title
+    :param y_label: specify particular y axis title
+    :param title: specify title
+    :return: Nothing. Modifies the g parameter.
+    """
+
+    # set axis titles
+    if not display_x_label:
+        g.set_xlabels('')
+    elif x_label is not None:
+        g.set_xlabels(x_label)
+
+    if not display_y_label:
+        g.set_ylabels('')
+    elif y_label is not None:
+        g.set_ylabels(y_label)
+
+    # set rotation on x axis
+    if rotation is not None:
+        g.set_xticklabels(rotation=rotation)
+
+    # set title DOESNT WORK
+    if title is not None:
+        g.set_titles(title)
 
 
 # ----------------------- LICENCES ----------------------- #
@@ -143,12 +201,15 @@ def plot_licences_time(count_df: pd.core.frame.DataFrame,
     # if batch_size not specified : plot all newspapers on the same figure
     if batch_size is None:
         g = sns.catplot(x="decade", y="count", hue="access_rights", kind="bar", data=count_df, height=5, aspect=2)
-        g.set_xticklabels(rotation=30)
+        display_setup(g, rotation=30, display_x_label=False, y_label='Number of issues',
+                      title='Issue frequency per access right type.')
 
     # else plot by batches (no intelligent batching is done)
     else:
         assert (0 < batch_size and batch_size < 20), "Batch size must be between 1 and 19."
-        catplot_by_batch_np(count_df, np_ids, 'decade', 'count', 'access_rights', batch_size, rotation=30)
+        catplot_by_batch_np(count_df, np_ids, 'decade', 'count', 'access_rights', batch_size,
+                            rotation=30, display_x_label=False, y_label='Number of issues',
+                            title='Issue frequency per access right type.')
 
 
 def plot_licences_np(count_df: pd.core.frame.DataFrame,
@@ -165,9 +226,10 @@ def plot_licences_np(count_df: pd.core.frame.DataFrame,
     # if batch_size not specified : plot all newspapers on the same figure
     if batch_size is None:
         g = sns.catplot(x="newspaper_id", y="count", hue="access_rights", kind="bar", data=count_df, height=5, aspect=2)
-        g.set_xticklabels(rotation=30)
+        display_setup(g, rotation=30, y_label='Number of issues', title='Issue frequency per access right type.')
 
     # else plot by batches (no intelligent batching is done)
     else:
         assert (0 < batch_size and batch_size < 20), "Batch size must be between 1 and 19."
-        catplot_by_batch_np(count_df, np_ids, 'newspaper_id', 'count', 'access_rights', batch_size, rotation=30)
+        catplot_by_batch_np(count_df, np_ids, 'newspaper_id', 'count', 'access_rights', batch_size,
+                            y_label='Number of issues', rotation=30, title='Issue frequency per access right type.')
