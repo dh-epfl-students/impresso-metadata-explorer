d91306f3a8a54e9a6b4d9cc6c6c6d892dc5cbc22
Justine Weber <justine.weber@epfl.ch>
2019-11-19 15:49:16 +0100

    Smalls modifs to setup.py.

--- impresso-stats/setup.py
+++ impresso-stats/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="impresso_stats",
+    name="impresso_statfunc",
     version="0.0.1",
     author="Justine Weber",
     author_email="justine.weber@epfl.ch",
     description="Statistics functions on the impresso dataset.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dhlab-epfl-students/impresso-metadata-explorer/impresso-stats",
+    url="https://github.com/dhlab-epfl-students/impresso-metadata-explorer",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
     install_requires=[
