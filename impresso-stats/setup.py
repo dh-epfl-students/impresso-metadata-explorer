238026b7dd04d8f306454df911edcaafcf9b28c7
Justine Weber <justine.weber@epfl.ch>
2019-11-19 14:47:25 +0100

    setup file done.

---
+++ impresso-stats/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="impresso-stats",
+    version="0.0.1",
+    author="Justine Weber",
+    author_email="justine.weber@epfl.ch",
+    description="Statistics functions on the impresso dataset.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dhlab-epfl-students/impresso-metadata-explorer",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'dask',
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'pandas',
+        'PyMySQL',
+        'scipy',
+        'seaborn',
+        'SQLAlchemy',
+        'impresso-pycommons',
+    ],
+    dependency_links=[
+      'https://github.com/impresso/impresso-pycommons/@v0.12.0#egg=impresso-pycommons',
+      ]
+)
