0a3f83907cb8f6219464a7b4fa39a8a709497cae
Justine Weber <justine.weber@epfl.ch>
2019-11-19 15:07:51 +0100

    changed impresso-stats to impresso_stats.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="impresso-stats",
+    name="impresso_stats",
     version="0.0.1",
     author="Justine Weber",
     author_email="justine.weber@epfl.ch",
