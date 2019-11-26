c832ee74b6547f5090fbf6d2efd1712b7ea7b77a
Justine Weber <justine.weber@epfl.ch>
2019-11-26 09:43:11 +0100

    rm 'stats_func' from setup file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="impresso_statfunc",
+    name="impresso_stats",
     version="0.0.1",
     author="Justine Weber",
     author_email="justine.weber@epfl.ch",
